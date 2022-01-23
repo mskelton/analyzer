4a705cdc2d8140df92395ed256c64421101d9503
Mark Skelton <mdskelton99@gmail.com>
2022-01-22 19:44:25 -0600

    Improve workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,8 @@ import { Config } from "@playwright/test"
 const config: Config = {
   forbidOnly: !!process.env.CI,
   projects: [
-    {
-      name: "e2e",
-      testDir: "./test/spec/e2e",
-    },
-    {
-      name: "integ",
-      testDir: "./test/spec/integ",
-    },
+    { name: "e2e", testDir: "./test/spec/e2e" },
+    { name: "integ", testDir: "./test/spec/integ" },
   ],
 }
 
