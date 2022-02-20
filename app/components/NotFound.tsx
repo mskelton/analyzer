f749609ab7d588cd2d9304293d010d724733cb29
Mark Skelton <mdskelton99@gmail.com>
2022-02-20 07:35:08 -0600

    Better 404 page

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -6,27 +6,39 @@ export function NotFound() {
 
   return (
     <Document title={`${caught.status} ${caught.statusText}`}>
-      <div className="flex h-full flex-col items-center justify-center">
-        <div className="flex">
-          <h1 className="border-r-1 pr-3 text-4xl font-extrabold text-primary-600">
-            404
-          </h1>
-
-          <div className="max-w-md">
-            <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-              Sorry we couldn&rsquo;t find this page.
+      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-primary-600 sm:text-5xl">
+              404
             </p>
 
-            <p className="mb-8">
-              But dont worry, you can find plenty of other things on our
-              homepage.
-            </p>
-          </div>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+                  Page not found
+                </h1>
+
+                <p className="mt-1 text-base text-gray-500">
+                  Please check the URL in the address bar and try again.
+                </p>
+              </div>
+
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link className="btn-primary" to="/">
+                  Go back home
+                </Link>
+
+                <a
+                  className="btn-secondary"
+                  href="https://github.com/mskelton/analyzer/issues/new"
+                >
+                  Open an issue
+                </a>
+              </div>
+            </div>
+          </main>
         </div>
-
-        <Link className="btn-primary" to="/">
-          Back to homepage
-        </Link>
       </div>
     </Document>
   )
--- app/routes/__app/accounts.tsx
+++ app/routes/__app/accounts.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction } from "remix"
+import { Link, LoaderFunction } from "remix"
 import { getAccounts } from "~/api/accounts.server"
 import { AccountsTable } from "~/components/accounts/AccountsTable"
 
@@ -10,8 +10,12 @@ export default function Accounts() {
   return (
     <>
       <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto flex max-w-7xl items-center justify-between py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-gray-900">Accounts</h1>
+
+          <Link className="btn-primary" to="create">
+            Add account
+          </Link>
         </div>
       </header>
 
