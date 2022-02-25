e235c2c841d00644161a1c22f25fc74cbe9cc908
Mark Skelton <mdskelton99@gmail.com>
2022-02-24 22:16:40 -0600

    Fix seed

--- db/seed.js
+++ db/seed.js
@@ -3,18 +3,22 @@ db = db.getSiblingDB("analyzer")
 
 db.User.insert({
   arn: "arn:user:jii36zh8:",
+  createdAt: new Date(),
   email: "mdskelton99@gmail.com",
-  password: "$2b$10$/QBjegkftcQFSIS2soi/uOldyWzcib0V9Qpeu9ArCjPOAsaX5PHYS",
   name: "Mark Skelton",
+  password: "$2b$10$/QBjegkftcQFSIS2soi/uOldyWzcib0V9Qpeu9ArCjPOAsaX5PHYS",
+  updatedAt: new Date(),
 })
 
 db.Account.insert({
   arn: "arn:account:jii36zh8:ecowczk9",
-  userArn: "arn:user:jii36zh8:",
-  token: "74e8u3j1zucq",
-  name: "Growth account",
   broker: "OspreyFX",
+  createdAt: new Date(),
+  name: "Growth account",
+  token: "74e8u3j1zucq",
   type: "LIVE",
+  updatedAt: new Date(),
+  userArn: "arn:user:jii36zh8:",
 })
 
 db.Deal.createIndex({ accountArn: 1, ticket: 1 }, { unique: true })
