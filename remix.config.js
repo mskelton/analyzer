d6b392b4f8fb194188c16968a9c07f0218cc983b
Mark Skelton <mdskelton99@gmail.com>
2022-02-28 07:23:07 -0600

    Fix build command

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+console.log(process.env.NODE_ENV)
 module.exports = {
   // When running locally in development mode, we use the built in remix server.
   server: process.env.NODE_ENV === "production" ? "./server.js" : undefined,
