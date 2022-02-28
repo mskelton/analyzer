a72344c78eab73c592e5c37c01b9c0f1863a1b11
Mark Skelton <mdskelton99@gmail.com>
2022-02-28 08:05:32 -0600

    Recreate hash for every gravatar request

--- app/utils/gravatar.server.ts
+++ app/utils/gravatar.server.ts
@@ -1,10 +1,8 @@
 import { createHash } from "crypto"
 
-const md5 = createHash("md5")
-
 export function gravatar(email: string) {
   const cleanedEmail = email.trim().toLowerCase()
-  const hash = md5.update(cleanedEmail).digest("hex")
+  const hash = createHash("md5").update(cleanedEmail).digest("hex")
 
   return `https://www.gravatar.com/avatar/${hash}?d=identicon`
 }
