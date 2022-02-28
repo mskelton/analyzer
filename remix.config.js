c9b58cd320af0c83892230384cb1d6f9b7fd9e0d
Mark Skelton <mdskelton99@gmail.com>
2022-02-28 07:46:55 -0600

    Fix public path

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,9 @@
-/**
- * @type {import('@remix-run/dev/config').AppConfig}
- */
+const prod = process.env.NODE_ENV === "production"
+
+/** @type {import('@remix-run/dev/config').AppConfig} */
 module.exports = {
+  publicPath: prod ? "/analyzer/build/" : "/build/",
   // When running locally in development mode, we use the built in remix server.
-  server: process.env.NODE_ENV === "production" ? "./server.js" : undefined,
+  server: prod ? "./server.js" : undefined,
   serverBuildTarget: "vercel",
 }
